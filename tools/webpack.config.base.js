693ae9e5397367a04c83dd30c170d561a25e5e92
Ivan Pilyugin <ivan.pilyugin@confirmit.com>
2018-01-18 09:43:54 +0300

    updated dependencies and serve as umd

--- tools/webpack.config.base.js
+++ tools/webpack.config.base.js
@@ -1,19 +1,20 @@
-var packageJSON = require('../package.json');
-var name = packageJSON["name"];
+const name = require('../package.json').name;
 
 module.exports = {
   entry: {
-    js:"./src/main.js",
+    js: './src/main.js',
   },
   output: {
-    path: "./dist",
-    filename: name+".bundle.js"
+    path: './dist',
+    filename: name + '.bundle.js',
+    libraryTarget: 'umd',
+
   },
   module: {
     loaders: [{
       test: /\.js$/,
-      exclude: /(bower_components)/,
-      loader: 'babel',
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
     }],
   },
   resolve: {
